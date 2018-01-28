5dab5ba131184380779b9014da5dd79afcd36689
Sam~Lubbers <lubbers.sc@gmail.com>
2018-01-28 08:04:13 +0000

    svg line

--- routes/shapes.js
+++ routes/shapes.js
@@ -3,5 +3,7 @@ var router = express.Router();
 
 router.get('/rectangle', (req, res) => {res.render('shapes/rectangle');});
 router.get('/circle', (req, res) => {res.render('shapes/circle');});
+router.get('/ellipse', (req, res) => {res.render('shapes/ellipse');});
+router.get('/line', (req, res) => {res.render('shapes/line');});
 
 module.exports = router;
