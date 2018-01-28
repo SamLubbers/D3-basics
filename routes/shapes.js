adf49f97351ac1ad8108c5b4ab2df46c432c40b9
Sam~Lubbers <lubbers.sc@gmail.com>
2018-01-28 09:15:44 +0000

    svg paths

--- routes/shapes.js
+++ routes/shapes.js
@@ -7,4 +7,6 @@ router.get('/ellipse', (req, res) => {res.render('shapes/ellipse');});
 router.get('/line', (req, res) => {res.render('shapes/line');});
 router.get('/polygon', (req, res) => {res.render('shapes/polygon');});
 router.get('/polyline', (req, res) => {res.render('shapes/polyline');});
+router.get('/path', (req, res) => {res.render('shapes/path');});
+
 module.exports = router;
