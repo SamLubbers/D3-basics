aa8d9391b7aa8657488bd7d87febcf10649a5bdb
Sam~Lubbers <lubbers.sc@gmail.com>
2018-01-28 08:22:43 +0000

    svg poligon

--- routes/shapes.js
+++ routes/shapes.js
@@ -5,5 +5,6 @@ router.get('/rectangle', (req, res) => {res.render('shapes/rectangle');});
 router.get('/circle', (req, res) => {res.render('shapes/circle');});
 router.get('/ellipse', (req, res) => {res.render('shapes/ellipse');});
 router.get('/line', (req, res) => {res.render('shapes/line');});
+router.get('/polygon', (req, res) => {res.render('shapes/polygon');});
 
 module.exports = router;
