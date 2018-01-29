35074ac9552a34288d3be9d239811a025f884005
SamLubbers <lubbers.sc@gmail.com>
2018-01-29 08:26:40 +0000

    dynamic style binding to HTML depending on data values

--- public/js/fundamentals.js
+++ public/js/fundamentals.js
@@ -7,4 +7,20 @@ var el = d3.select('body')
 				.append('p')
 				.text(function(d){
 					return 'this paragraph is binded to the value ' + d
+				}).attr('class', function(d){
+					if(d>3){
+						return 'foo'
+					}else{
+						return null
+					}
+				})
+				.classed('bar', function(d){
+					return d < 3;
+				})
+				.style('color', function(d){
+					if (d > 3){
+						return 'red';
+					}else{
+						return 'blue';
+					}
 				});
