91049ea36cc72fd619ee39efb4a00ad531ec2fd2
SamLubbers <lubbers.sc@gmail.com>
2018-01-29 08:41:31 +0000

    data loading from csv

--- public/js/fundamentals.js
+++ public/js/fundamentals.js
@@ -1,26 +1,33 @@
-var dataset = [1, 2, 3, 4, 5]
+d3.csv('../data/example_csv.csv', function(err, data){
+  if(err){
+    console.log(err);
+  }
+  generate(data.columns);
+});
 
-var el = d3.select('body')
-				.selectAll('p')
-				.data(dataset)
-				.enter()
-				.append('p')
-				.text(function(d){
-					return 'this paragraph is binded to the value ' + d
-				}).attr('class', function(d){
-					if(d>3){
-						return 'foo'
-					}else{
-						return null
-					}
-				})
-				.classed('bar', function(d){
-					return d < 3;
-				})
-				.style('color', function(d){
-					if (d > 3){
-						return 'red';
-					}else{
-						return 'blue';
-					}
-				});
+function generate(dataset){
+	d3.select('body')
+	.selectAll('p')
+	.data(dataset)
+	.enter()
+	.append('p')
+	.text(function(d){
+		return 'this paragraph is binded to the value ' + d
+	}).attr('class', function(d){
+		if(d>3){
+			return 'foo'
+		}else{
+			return null
+		}
+	})
+	.classed('bar', function(d){
+		return d < 3;
+	})
+	.style('color', function(d){
+		if (d > 3){
+			return 'red';
+		}else{
+			return 'blue';
+		}
+	});
+}
