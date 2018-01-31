b313b6c41c9e1d4db86fa0f126e0d5b835d6b97b
SamLubbers <lubbers.sc@gmail.com>
2018-01-31 08:41:28 +0000

    added labels to bar graph

--- public/js/charts.js
+++ public/js/charts.js
@@ -4,13 +4,14 @@ var dataset = [];
 for( var i = 0; i<25;i++){
   dataset[i] = Math.round(Math.random() * 50)
 }
-chart_height = 400
-chart_width = 800
-
-var chart = d3.select('#chart');
 
 // create svg container
-var svg = chart.append('svg')
+chart_height = 400
+chart_width = 800
+bar_padding = 1 // spacing between bars
+
+var svg = d3.select('#chart')
+  .append('svg')
   .attr('height', chart_height)
   .attr('width', chart_width)
 
@@ -30,10 +31,31 @@ svg.selectAll('rect')
   })
   .attr('width', function(d){
     // set the bars width dynamically adjusted to the data
-    return (chart_width / dataset.length) - 1;
+    return (chart_width / dataset.length) - bar_padding;
   })
   .attr('height', function(d){
     // set the bars height dynamically adjusted to the data
     return d * chart_height / Math.max(...dataset);
   })
   .attr('fill', '#01FF70');
+
+// create labels
+svg.selectAll('text')
+  .data(dataset)
+  .enter()
+  .append('text')
+  .text(function(d){
+    return d
+  })
+  .attr('x', function(d, i){
+    // center text in the chart
+    bar_position = i * (chart_width / dataset.length);
+    bar_middle = ((chart_width/dataset.length) - bar_padding) / 2;
+    return bar_position + bar_middle;
+  })
+  .attr('y', function(d){
+    // set text inside charts
+    return chart_height - (d * chart_height / Math.max(...dataset)) + 18;
+  })
+  .attr('fill', '#777')
+  .attr('text-anchor', 'middle');
