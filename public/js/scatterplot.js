704855b6045bc4197e8d5ca8a916761ed04f8a6d
SamLubbers <lubbers.sc@gmail.com>
2018-02-01 08:15:16 +0000

    linear scales to make data fit inside graph

--- public/js/scatterplot.js
+++ public/js/scatterplot.js
@@ -10,12 +10,26 @@ var dataset            =   [
 ];
 var chart_width     =   800;
 var chart_height    =   400;
-var radius = 5
+var radius = 5;
+var padding = 50;
+
 // create svg element
 var svg = d3.select('#chart')
   .append('svg')
   .attr('width', chart_width)
   .attr('height', chart_height);
+
+x_values = dataset.map(x => x[0]);
+y_values = dataset.map(x => x[1]);
+
+// create scales
+x_scale = d3.scaleLinear()
+    .domain([0,d3.max(x_values)])
+    .range([padding, chart_width - padding]);
+
+y_scale = d3.scaleLinear()
+    .domain([0,d3.max(y_values)])
+    .range([chart_height - padding, padding]);
 
 // create circles
 svg.selectAll('circle')
@@ -23,12 +37,10 @@ svg.selectAll('circle')
   .enter()
   .append('circle')
   .attr('cx', function(d){
-    x_values = dataset.map(x => x[0]);
-    return d[0] * (chart_width - radius * 2) / Math.max(...x_values);
+    return x_scale(d[0]);
   })
   .attr('cy', function(d){
-    y_values = dataset.map(x => x[1]);
-    return d[1] * (chart_height - radius * 2) / Math.max(...y_values);
+    return y_scale(d[1]);
   })
   .attr('r', radius)
   .attr('fill', '#444')
