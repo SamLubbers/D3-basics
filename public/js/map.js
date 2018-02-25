80496b02ea36be310af70f8007cff10ba919039e
Sam~Lubbers <lubbers.sc@gmail.com>
2018-02-25 14:49:46 +0000

    cloropleth map

--- public/js/map.js
+++ public/js/map.js
@@ -17,13 +17,41 @@ svg = d3.select('#chart-L')
 	.attr('width', chart_width)
 	.attr('height', chart_height);
 
-d3.json('../data/us-geo/us.json', function(dataset) {
-	svg.selectAll('path')
-		.data(dataset.features)
-		.enter()
-		.append('path')
-		.attr('d', geo_path)
-		.attr('fill', 'white')
-		.attr('stroke', '#4285F4')
-		.attr('stroke-width', 1);
+d3.json('../data/us-geo/us-flu.json', function(us_flu_dataset) {
+	map_colors.domain([
+		d3.min(us_flu_dataset, (d) => d.num),
+		d3.max(us_flu_dataset, (d) => d.num)
+	]);
+
+	d3.json('../data/us-geo/us.json', function(us_dataset) {
+		us_dataset.features.forEach(function(us_e, us_i) {
+			state = us_e.properties.name;
+			us_flu_dataset.forEach(function(flu_e) {
+				if (flu_e.state === state) {
+					us_dataset.features[us_i].properties.flu_risk = flu_e.num;
+				}
+			})
+		})
+		svg.selectAll('path')
+			.data(us_dataset.features)
+			.enter()
+			.append('path')
+			.attr('d', geo_path)
+			.attr('fill', function(d) {
+				if (d.properties.flu_risk) {
+					return map_colors(d.properties.flu_risk)
+				} else {
+					return '#DFDFDF'
+				}
+			})
+			.attr('stroke', function(d) {
+				if (d.properties.flu_risk) {
+					return '#4285F4';
+				} else {
+					return '#DFDFDF';
+				}
+			})
+			.attr('stroke-width', 1);
+	});
+
 });
